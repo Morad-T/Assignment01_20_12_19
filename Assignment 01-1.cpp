4881901a0e484fe4827db8609ba0118619c81ed6
Morad-T <61923896+Morad-T@users.noreply.github.com>
2020-12-19 22:34:51 +0200

    Update Assignment 01-1.cpp

--- Assignment 01-1.cpp
+++ Assignment 01-1.cpp
@@ -6,7 +6,7 @@ int main()
 	unsigned long long int Factorial;
 	int N, i;
 	Factorial = 1;
-	cout << "Enter Number To Be Factorialized (Not more than 20 or a buffer overflow will occur) \n";
+	cout << "Enter a Number To Calculate its Factorial (Not more than 20 or a buffer overflow will occur) \n";
 	cin >> N;
 	for (i = N; i > 1; i--)
 		Factorial = Factorial * i;
